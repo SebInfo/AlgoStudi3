3975735faf19d2a11587c2376624232f99425811
SebInfo <contact@inion.info>
2023-06-06 08:23:26 +0200

    Modification du code

--- renduMonnaie.py
+++ renduMonnaie.py
@@ -1,6 +1,8 @@
 valeurs = [1,2,5,10,20,50,100,200,500]
 
-def renduMonnaie(sommeARendre):
+#Entrées : Ensemble P d’entiers (valeurs), entier s (sommeAREndre)
+#Sortie  : Liste de pièces de P, minimale, dont la somme vaut s
+def renduMonnaie(valeurs, sommeARendre):
     liste = []
     indice = len(valeurs) - 1
 
@@ -14,4 +16,5 @@ def renduMonnaie(sommeARendre):
     
     return liste
 
-print(renduMonnaie(7))
+euro = [1,2,5,10,20,50,100,200,500]
+print(renduMonnaie(euro,7))
