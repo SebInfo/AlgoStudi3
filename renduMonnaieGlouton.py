c47bc79a7f85954381831f6137d53ee0d3226885
SebInfo <contact@inion.info>
2023-06-07 15:30:10 +0200

    ajout du PDF

--- renduMonnaieGlouton.py
+++ renduMonnaieGlouton.py
@@ -1,13 +1,13 @@
 valeurs = [1,2,5,10,20,50,100,200,500]
 
-#Entrées : Ensemble P d’entiers (valeurs), entier s (sommeAREndre)
+#Entrées : Ensemble P d’entiers croissants (valeurs), entier s (sommeAREndre)
 #Sortie  : Liste de pièces de P, minimale, dont la somme vaut s
 def renduMonnaie(valeurs, sommeARendre):
     liste = []
     indice = len(valeurs) - 1
 
     while sommeARendre > 0:
-        piece = valeurs[indice] #glouton
+        piece = valeurs[indice] #glouton on commence par la plus grande pièce ou billet
         if piece > sommeARendre:
             indice -= 1
         else:
